6d994fe9f5e73349156b270332e6a83987140630
Jan Redmann <janredmann1@gmail.com>
2021-04-08 17:46:02 +0200

    add store a new value

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,11 @@ module.exports = async function main(callback) {
         // set up truffle contract representing the deployed box contract
         const Box = artifacts.require('Box');
         const box = await Box.deployed();
+
+        // replace the value with a new value
+        await box.store(23);
+
+        // retrieve the current value 
         const value = await box.retrieve();
         console.log('Current value of the box is: ', value.toString());
 
