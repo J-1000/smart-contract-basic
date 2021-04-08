de5241b0bb9fbbff94d0b9f7d28ad01da11ded6e
Jan Redmann <janredmann1@gmail.com>
2021-04-08 15:35:37 +0200

    add ownable contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,22 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "./access-control/Auth.sol";
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Box {
+// Make Box inherit from the Ownable contract
+contract Box is Ownable {
     uint256 private value;
-    Auth private auth;
 
     event ValueChanged(uint256 newValue);
 
-    constructor(Auth _auth)  {
-        auth = _auth;
-    }
-
-    function store(uint256 newValue) public {
-        // Require that the caller is registered as an administrator in Auth
-        require(auth.isAdministrator(msg.sender), "Unauthorized");
-
+    // The onlyOwner modifier restricts who can call the store function
+    function store(uint256 newValue) public onlyOwner {
         value = newValue;
         emit ValueChanged(newValue);
     }
