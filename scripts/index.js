d2a27767f2e35fa5bb8e470788d32ae4c2bea081
Jan Redmann <janredmann1@gmail.com>
2021-04-08 17:43:42 +0200

    add retrieve value script

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,15 @@
 module.exports = async function main(callback) {
     try {
+        // retrieve accounts from local node
         const accounts = await web3.eth.getAccounts();
         console.log(accounts);
+
+        // set up truffle contract representing the deployed box contract
+        const Box = artifacts.require('Box');
+        const box = await Box.deployed();
+        const value = await box.retrieve();
+        console.log('Current value of the box is: ', value.toString());
+
         callback(0)
     } catch (error) {
         console.error(error);
