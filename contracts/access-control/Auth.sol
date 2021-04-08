60462d3db8840075b8eb4532fcd73433c91ddddc
Jan Redmann <janredmann1@gmail.com>
2021-04-08 15:31:38 +0200

    add auth contract

---
+++ contracts/access-control/Auth.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Auth {
+    address private administrator;
+
+    constructor() {
+        // Make the deployer of the contract the administrator
+        administrator = msg.sender;
+    }
+
+    function isAdministrator(address user) public view returns (bool) {
+        return user == administrator;
+    }
+}
